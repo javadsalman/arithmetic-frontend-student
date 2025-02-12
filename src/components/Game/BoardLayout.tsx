bfea1696cb0b8f31529f80ece0df932988451411
cavadsalman <cavad2013@gmail.com>
2025-02-12 10:05:52 +0100

    board padding responsive

--- src/components/Game/BoardLayout.tsx
+++ src/components/Game/BoardLayout.tsx
@@ -7,7 +7,7 @@ interface BoardLayoutProps {
 
 function BoardLayout({children, boardRef, className=""}: BoardLayoutProps) {
     return (
-        <div className='relative w-full h-full rounded-lg p-4 md:p-8 shadow-2xl bg-orange-400 border border-orange-500' ref={boardRef}>
+        <div className='relative w-full h-full rounded-lg p-4 lg:p-8 shadow-2xl bg-orange-400 border border-orange-500' ref={boardRef}>
             <div className={`items-center h-full bg-emerald-600 overflow-hidden rounded-sm border border-green-700 ${className}`}>
                 {children}
             </div>
